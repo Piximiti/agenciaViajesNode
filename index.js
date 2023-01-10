1e6ea8ecbf087deac8434b40e3b026e86d6f23fa
Santiago Trujillo <santitm26@hotmail.com>
2023-01-10 14:52:06 -0500

    primer comit

---
+++ index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import router from './routes/index.js';
+import db from './config/db.js';
+
+const app = express();
+
+db.authenticate()
+    .then( () => console.log('Base de datos conectada'))
+    .catch(error => console.log(error));
+
+//Definir Puerto
+const port = process.env.PORT || 4000;
+
+//Habilitar PUG
+app.set('view engine', 'pug');
+
+//Obtener el año actual
+app.use((req, res, next) => {
+    const year = new Date();
+    res.locals.actualYear = year.getFullYear();
+    res.locals.nombresitio = "Agencia de Viajes";
+    next();
+});
+
+app.use(express.urlencoded({extended: true}));
+
+//Definir la carpeta pública
+app.use(express.static('public'))
+
+//Agregar el router
+app.use('/', router);
+
+app.listen(port, () => {
+    console.log(`El servidor esta funcionando en el puerto ${port}`)
+});
